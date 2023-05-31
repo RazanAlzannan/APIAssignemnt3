7df6112f88ada5bff1604ab9d21253e29c302333
Razan <razanalzannan@gmail.com>
2023-06-01 01:01:10 +0300

    Initial Commit

---
+++ APIAssignment3/APIAssignment3App.swift
@@ -0,0 +1,17 @@
+//
+//  APIAssignment3App.swift
+//  APIAssignment3
+//
+//  Created by Razan Mohammed Alzannan on 12/11/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct APIAssignment3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
