0dbaabc81e66e6f7c59edc2fe6cf60630a1e0c68
Razan <razanalzannan@gmail.com>
2023-06-01 09:49:34 +0300

    Updated

---
+++ APIAssignment3/Books.swift
@@ -0,0 +1,20 @@
+//
+//  Books.swift
+//  APIAssignment3
+//
+//  Created by Razan Mohammed Alzannan on 12/11/1444 AH.
+//
+
+import SwiftUI
+
+struct Books: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Books_Previews: PreviewProvider {
+    static var previews: some View {
+        Books()
+    }
+}
